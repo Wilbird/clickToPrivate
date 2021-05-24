00fd6a10df42dcc6a7e0e2295877e50644584c82
Wilbird <Wilbird@users.noreply.github.com>
2021-05-24 22:28:19 +0200

    Ctrl + Shift + Click should not be affected by the addon (#2)

---
+++ content.js
@@ -0,0 +1,29 @@
+var target;
+
+window.addEventListener('click', handler, false);
+
+function handler(event) {
+    if (!event.shiftKey) {
+        return;
+    }
+    if (event.ctrlKey) {
+        return;
+    }
+    // left click ?
+    if (event.button !== 0 &&
+        event.buttons !== 0) {
+        return;
+    }
+        
+    target = event.target;
+    
+    while (target) {
+        if (target.tagName && target.tagName === "A") {
+            event.preventDefault();
+            event.stopPropagation();
+            var sending = browser.runtime.sendMessage({"url": target.href});
+            break;
+        }
+        target = target.parentNode;
+    }
+};
